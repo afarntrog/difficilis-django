fe6d153a097ae4e1f50f7d808bae226e62af8fc5
AaronF <aaronfarntrog@gmail.com>
2019-10-16 19:43:40 -0400

    updated settings, secret key

--- difficilis/settings.py
+++ difficilis/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '$_v727yr4*msrkr2)mzolcppy&j8urkoo-@4ogv&=n_ly$$8hj'
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True') # False
 
 ALLOWED_HOSTS = ['*']#['difficilis.herokuapp.com','.difficilis.com']
 
