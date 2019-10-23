d0e183619dd398e8692b37bf7adc8670040a91f0
AaronF <aaronfarntrog@gmail.com>
2019-10-22 20:56:52 -0400

    added enhanced admin features

--- easydilemma/admin.py
+++ easydilemma/admin.py
@@ -3,8 +3,11 @@ from .models import *
 # Register your models here.
 
 # admin.site.register(DilemmaModel)
-# admin.site.register(Reason)
-admin.site.register(Dilemma)
+# admin.site.register(Reason)\
+class DilemmaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_date',)
+
+admin.site.register(Dilemma, DilemmaAdmin)
 admin.site.register(DilemmaPartOne)
 admin.site.register(DilemmaPartTwo)
 admin.site.register(ReasonPartOne)
